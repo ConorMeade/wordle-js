90602aeafa4da231f81007c62a5eb757ef32880c
Conor Meade <cmeade6479@gmail.com>
2024-07-08 15:41:51 -0400

    document functions, update and clear letter board

--- getWordleWord.js
+++ getWordleWord.js
@@ -14,19 +14,19 @@
 
 
 // async function getWordleWord() {
-//     // const url = "https://random-word-api.herokuapp.com/word?length=5";
-//     // try {
-//     //     const response = await fetch(url);
-//     //     if (!response.ok) {
-//     //     throw new Error(`Response status: ${response.status}`);
-//     //     }
-    
-//     //     const json = await response.json();
-//     //     console.log(json[0]);
-//     //     return json[0]
-//     // } catch (error) {
-//     //     console.error(error.message);
-//     // }
+//     const url = "https://random-word-api.herokuapp.com/word?length=5";
+//     try {
+//         const response = await fetch(url);
+//         if (!response.ok) {
+//             throw new Error(`Response status: ${response.status}`);
+//         }
+//         const json = await response.json();
+//         console.log(json[0]);
+//         return json[0]
+//     } catch (error) {
+//         console.error(error.message);
+//         // return 'brass'
+//     }
 //     return 'ounce'
 // }
 
--- index.js
+++ index.js
@@ -10,39 +10,78 @@ function Cell(letter, colorCode){
     this.colorCode = colorCode
 }
 
-function updateBoard(newCells, currentRow, word){
+function updateBoard(newCells, currentRow){
+    // function used to update the regular game board and the letter board.
     let parentDiv = document.getElementById('row' + currentRow);
     let greenCount = 0;
     while (parentDiv.firstChild) {
         parentDiv.firstChild.remove();
     }
-    let divCells = []
+    let divCells = [];
+    let usedLetters = '';
     newCells.forEach((c) => {
         divCells.push(makeDivCell(c));
+
+        // updating letter board
+        let letterBoardCellDiv = document.getElementById(`letterBoard-${c.letter.toUpperCase()}`)
+        if(letterBoardCellDiv != null) {
+            switch(c.colorCode) {
+                case 1:
+                    letterBoardCellDiv.className = 'letterBoardCellGreen';
+                    
+                    usedLetters += c.letter
+                    // mark id as answered so we do not overwrite
+                    letterBoardCellDiv.id = `letterBoardAnswered-${c.letter.toUpperCase()}`;
+                    break;
+                case 2:
+                    letterBoardCellDiv.className = 'letterBoardCellYellow';
+                    usedLetters += c.letter
+                    break;
+                case 3:
+                    /*
+                        When we have a double yellow letter, we do not want to overwrite the second 
+                        instance of the letter as gray when that letter is present in the word
+
+
+                        i.e. if the word is 'brass' and the guess is 'canal' (two a's)
+
+                        'a' is a gold letter, so it should be marked like that.
+                        We keep track of usedLetters so the second a in canal (which is not in brass)
+                        is a miss. However, that second 'a' should not overwrite the yellow letter on the letter board.
+
+                    */
+                    if (!usedLetters.includes(c.letter)) {
+                        letterBoardCellDiv.className = 'letterBoardCellGray';
+                    }
+                    usedLetters += c.letter
+                    break;
+                default:
+                    letterBoardCellDiv.className = "letterBoardCellUnanswered"
+                    console.log('reached an invalid colorCode (letter board)');
+                
+            }
+        }
     })
 
-
-    for(let i=0; i < newCells.length; i++){
-        if(newCells[i].colorCode == 1){
-            greenCount++
-        }
-
-        parentDiv.appendChild(divCells[i])
+    // counting green cells
+    for(let i=0; i < divCells.length; i++){
+        parentDiv.appendChild(divCells[i]);
     }
 }
 
 function countGreenCells(newCells){
-    let greenCount = 0
+    // keep track of how many green letters on a given guess. 5 greens is a correct guess
+    let greenCount = 0;
     for(let i=0; i < newCells.length; i++){
         if(newCells[i].colorCode == 1){
-            greenCount++
+            greenCount++;
         }
     }
-
-    return greenCount
+    return greenCount;
 }
 
 function initRows(){
+    // init game board with Cell objects/divs
     let rowCells = [];
     for(let i=0; i<5; i++){
         rowCells.push(new Cell(" ", 0));
@@ -57,14 +96,15 @@ function initRows(){
 }
 
 function initLetterBoard(letterBoardRowLetters, firstLetter){
+    // init letterboard, cells are gray at first
     let letterBoardCells = []
     letterBoardRowLetters.forEach((letter) => {
-        letterBoardCells.push(new Cell(letter, 4))
+        letterBoardCells.push(new Cell(letter, 4));
     })
 
     let letterBoardDivCells = []
     letterBoardCells.forEach((c) => {
-        letterBoardDivCells.push(makeDivCell(c))
+        letterBoardDivCells.push(makeDivCell(c));
     })
 
     let currentDiv = document.getElementById(firstLetter + 'Row')
@@ -74,22 +114,28 @@ function initLetterBoard(letterBoardRowLetters, firstLetter){
 }
 
 function getCellsForGuess(guessedWord, actualWord){
+    // take guessed word and compare with actual word, create new cells for current row with proper colors
     let newRow = [];
+    let usedLetters = '';
     for (let i = 0; i < actualWord.length; i++) {
         let curLetter = guessedWord.charAt(i);
         let actualWordNoCurLetter = actualWord.replace(actualWord.charAt(i), "");
         if(actualWord.charAt(i) == curLetter){
             newRow.push(new Cell(curLetter.toUpperCase(), 1));
-        } else if (actualWord.charAt(i) != guessedWord.charAt(i) && actualWordNoCurLetter.indexOf(curLetter) > -1){
+            usedLetters += curLetter;
+        } else if (actualWord.charAt(i) != guessedWord.charAt(i) && actualWordNoCurLetter.indexOf(curLetter) > -1 && !usedLetters.includes(curLetter)){
             newRow.push(new Cell(curLetter.toUpperCase(), 2));
+            usedLetters += curLetter;
         } else {
             newRow.push(new Cell(curLetter.toUpperCase(), 3));
+            usedLetters += curLetter;
         }
     }
     return newRow
 }
 
 function makeDivCell(cell) {
+    // parse Cell object to make a div, add new div as child to that guess's row
     const newDiv = document.createElement('div');
     newDiv.style.cssText = 'color: black;border-style: solid;height: 90px;width: 90px;';
 
@@ -104,19 +150,19 @@ function makeDivCell(cell) {
             newDiv.className = 'yellowCell';
             break;
         case 3:
-            newDiv.className = 'whiteCell';
+            newDiv.className = 'grayCell';
             break;
         case 4:
             newDiv.style.cssText = 'color: black;border-style: solid;height: 55px;width: 55px;';
-            newDiv.className = "letterBoardCell"
-            newDiv.id = `letter-${cell.letter}`
+            newDiv.className = 'letterBoardCellUnanswered';
+            newDiv.id = `letterBoard-${cell.letter.toUpperCase()}`;
             break;
         default:
             console.log('reached an invalid colorCode');
         
     }
     const newContent = document.createTextNode(cell.letter);
-    newDiv.appendChild(newContent)
+    newDiv.appendChild(newContent);
     return newDiv;
 }
 
@@ -144,6 +190,7 @@ async function checkValidWord(guessedWord) {
 
 
 function clearBoard(){
+    // reset button function, remove all divs and re-init board
     for(let i=0; i<6;i++){
         let parentDiv = document.getElementById('row' + i);
         while (parentDiv.firstChild) {
@@ -159,5 +206,21 @@ function clearBoard(){
             currentDiv.appendChild(element);
         });
     }
+
+    const alphabet = [
+        "A", "B", "C", "D", "E", "F", 
+        "G", "H", "I", "J", "K", "L",
+        "M", "N", "O", "P", "Q", "R",
+        "S", "T", "U", "V", "W", "X",
+        "Y", "Z", 
+    ];
+
+    alphabet.forEach((letter) => {
+        let letterBoardCellDiv = document.getElementById(`letterBoard-${letter}`);
+        if (letterBoardCellDiv == null) {
+            letterBoardCellDiv = document.getElementById(`letterBoardAnswered-${letter}`);
+        }
+        letterBoardCellDiv.className = 'letterBoardCellUnanswered';
+    })
     
 }
